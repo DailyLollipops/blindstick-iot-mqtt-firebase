e67328f1b8a6f72d101c180f39a01032506a18a7
DailyLollipops <clarencemadrigal84@gmail.com>
2024-07-13 03:10:34 +0800

    Server update - updated device parameters - switch to single device only - utilize firebase realtime database for synchronizing parameters and settings - utilize firestore database for stats metrics

--- README.md
+++ README.md
@@ -10,6 +10,7 @@ A publisher based implementation of MQTT to update firestore database collection
 4. Install requirements with `pip install -r requirements.txt`
 
 ### Setting up mosquito on AWS
+
 1. Start an EC2 ubuntu instance
 2. Open and assign the following ports in security group:
    - 1883
@@ -18,6 +19,7 @@ A publisher based implementation of MQTT to update firestore database collection
    - 8081
 3. Assign an elastic IP to the instance
 4. Edit mosquitto configuration
+
    ```bash
    sudo nano /etc/mosquitto/mosquitto.conf
 
@@ -25,6 +27,7 @@ A publisher based implementation of MQTT to update firestore database collection
    listener 1883 0.0.0.0
    allow_anonymous true
    ```
+
 5. Restart mosquitto with `sudo systemctl restart mosquitto`
 6. Test with `${ELASTIC_IP}:1883`
 
@@ -33,7 +36,7 @@ A publisher based implementation of MQTT to update firestore database collection
 To run the server, make sure the MQTT broker is up and running, then run:
 
 ```bash
-python server.py -t blindstick
+python server.py -t server
 ```
 
 where the `-t` argument is the topic subscribed on by the blindstick devices
@@ -43,11 +46,11 @@ where the `-t` argument is the topic subscribed on by the blindstick devices
 You can run the publish script by emulating the data sent by the blindstick devices using
 
 ```bash
-python publish.py -h localhost -t blindstick -p "1 0 1 16.1 121.34221 77.4343"
+python publish.py -h localhost -t server -p "1 0 1 1 121.34221 77.4343"
 ```
 
 where:
 
 - -h = mqtt broker address
 - -t = topic subscribed
-- -p = payload - Payload format message: '{id} {balance} {obstacle} {water} {lat} {lon}'
+- -p = payload - Payload format message: '{emergency} {fall} {obstacle} {water} {lat} {lon}'
--- server.py
+++ server.py
@@ -1,6 +1,9 @@
-from firebase_admin import credentials, firestore, messaging
+from firebase_admin import credentials, firestore
+from firebase_admin import db as firebase
+from datetime import datetime, timezone
 import firebase_admin
 import paho.mqtt.client as mqtt
+import paho.mqtt.publish as publish
 import click
 import traceback
 
@@ -18,47 +21,83 @@ def run(**args):
     silent = args.pop("silent")
 
     cred = credentials.Certificate('credentials.json') 
-    app = firebase_admin.initialize_app(cred)
-    db = firestore.client()
-    device_collection = db.collection("devices")
+    firebase_admin.initialize_app(cred, {
+        "databaseURL": "https://blindstick-e0f8d-default-rtdb.asia-southeast1.firebasedatabase.app/"
+    })
+    parameter_reference = firebase.reference("device/parameters")
+    setting_reference = firebase.reference("device/settings")
+    firestore_db = firestore.client()
+    trigger_collection = firestore_db.collection("triggers")
+
+    def on_settings_change(event: firebase.Event):
+        print(f"Setting listener triggered")
+        settings = setting_reference.get()
+        print(f"Got settings: {settings}")
+        beep = settings.get("beep")
+        fall_intensity_threshold = settings.get("fall_intensity_threshold")
+        sound = settings.get("sound")
+        msg = f"{int(beep)} {fall_intensity_threshold} {int(sound)}"
+        publish.single("blindstick", msg, hostname=host)
 
     def on_connect(client, userdata, flags, reason_code, properties):
         print(f"Connected with result code: {reason_code}")
         client.subscribe(topic)
 
     def on_message(client, userdata, msg):
-        # Payload format message: '{id} {balance} {obstacle} {water} {lat} {lon}'
+        # Payload format message: '{emergency} {fall} {obstacle} {water} {lat} {lon}'
         payload = bytes.decode(msg.payload)
-        print(f"Received: {payload}")
+        print(f"Received parameters: {payload}")
 
         try:
             data = payload.split()
-            device_id = data[0]
-            balance = bool(float(data[1]))
+            emergency = bool(float(data[0]))
+            fall = bool(float(data[1]))
             obstacle = bool(float(data[2]))
-            water = float(data[3])
+            water = bool(float(data[3]))
             latitude = data[4]
             longitude = data[5]
-            document = device_collection.document(device_id)
-            document_data = {
-                "balance": balance,
+            parameters = {
+                "emergency": emergency,
+                "fall": fall,
                 "obstacle": obstacle,
                 "water": water,
                 "latitude": latitude,
                 "longitude": longitude
             }
-            document.update(document_data)
-            print(f"Successfully updated data on device ID: {device_id}")
+            parameter_reference.update(parameters)
+            if emergency:
+                trigger_collection.add({
+                    "source": "emergency",
+                    "created_at": datetime.now(tz=timezone.utc)
+                })
+            elif fall:
+                trigger_collection.add({
+                    "source": "fall",
+                    "created_at": datetime.now(tz=timezone.utc)
+                })
+            elif obstacle:
+                trigger_collection.add({
+                    "source": "obstacle",
+                    "created_at": datetime.now(tz=timezone.utc)
+                })
+            elif water:
+                trigger_collection.add({
+                    "source": "water",
+                    "created_at": datetime.now(tz=timezone.utc)
+                })
+            print(f"Successfully updated data")
         except:
             print("Error occured")
             if not silent:
                 print(traceback.format_exc())
 
+    setting_listener = setting_reference.listen(on_settings_change)
     mqttc = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
     mqttc.on_connect = on_connect
     mqttc.on_message = on_message
     mqttc.connect(host, port, keepalive)
     mqttc.loop_forever()
+
 
 if __name__ == "__main__":
     run()
